6233c0e9d2415874b94842d88c1a9335e2f6fe89
root <bitaron90@gmail.com>
2016-10-02 21:39:32 +0600

    Add error and console log to master branch and refactor hibernate

--- src/test/java/com/bitaron/archtype/MyResourceTest.java
+++ src/test/java/com/bitaron/archtype/MyResourceTest.java
@@ -1,7 +1,11 @@
 package com.bitaron.archtype;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertFalse;
 import static org.junit.Assert.assertEquals;
 
 
@@ -9,6 +13,14 @@ public class MyResourceTest {
 
     @Test
     public void testDbConnection() {
-        assertEquals(true,true);
+        try {
+            SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+            Session session = sessionFactory.openSession();
+            assertEquals(session.isConnected(),true);
+        } catch (Throwable ex) {
+            System.out.println("Initial SessionFactory creation failed. " + ex);
+            ex.printStackTrace();
+            assertFalse("Initial SessionFactory creation failed. " + ex,true);
+        }
     }
 }
