e43c6f5303ae28bbbecfb98cf60fdd3961f1731c
root <bitaron90@gmail.com>
2016-10-02 19:14:42 +0600

    Deploy jax-rs using Application subclass and web.xml

---
+++ src/main/java/com/bitaron/archtype/BaseApplication.java
@@ -0,0 +1,8 @@
+package com.bitaron.archtype;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("webapi")
+public class BaseApplication extends Application{
+}
