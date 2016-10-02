4c906665dfc74be46b54f175aab562863a08f57b
root <bitaron90@gmail.com>
2016-10-02 19:50:56 +0600

    Remove hibernate

--- src/main/java/com/bitaron/archtype/resources/MyResource.java
+++ src/main/java/com/bitaron/archtype/resources/MyResource.java
@@ -18,6 +18,6 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "Seed app for jersey-hibernate!";
+        return "Seed app for jersey-security!";
     }
 }
--- src/test/java/com/bitaron/archtype/MyResourceTest.java
+++ src/test/java/com/bitaron/archtype/MyResourceTest.java
@@ -1,27 +1,14 @@
 package com.bitaron.archtype;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 
 
 public class MyResourceTest {
 
     @Test
     public void testDbConnection() {
-        try {
-            SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-            Session session = sessionFactory.openSession();
-            assertEquals(session.isConnected(),true);
-        } catch (Throwable ex) {
-            System.out.println("Initial SessionFactory creation failed. " + ex);
-            ex.printStackTrace();
-            assertFalse("Initial SessionFactory creation failed. " + ex,true);
-        }
+        assertEquals(true,true);
     }
-
 }
